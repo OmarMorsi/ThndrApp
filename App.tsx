ce7d6f1c783c7b19181c2b00d45e85754c67a7e4
Morsi <morsi@Morsis-MacBook-Pro.local>
2024-09-28 05:54:01 +0300

    fix native splash screen ios

--- App.tsx
+++ App.tsx
@@ -1,32 +1,17 @@
 import * as React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Tickers from './src/components/Tickers';
 import colors from './src/styles/colors';
 import Header from './src/components/Header';
-import SplashScreen from './src/screens/SplashScreen';
-import {useEffect, useState} from 'react';
 
 function App(): React.JSX.Element {
-  const [isSplashVisible, setIsSplashVisible] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsSplashVisible(false);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <View style={styles.outerContainer}>
       <View style={styles.headerSpacing}></View>
-      {isSplashVisible ? (
-        <SplashScreen />
-      ) : (
-        <>
-          <Header />
-          <Tickers />
-        </>
-      )}
+      <>
+        <Header />
+        <Tickers />
+      </>
     </View>
   );
 }
